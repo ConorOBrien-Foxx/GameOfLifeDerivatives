24480301be5849995692e1285381af9260aa1bae
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2022-03-25 23:28:01 -0400

    add compile, run, and Display

---
+++ Display.java
@@ -0,0 +1,98 @@
+package GameOfLifeDerivatives;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Display extends JComponent {
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int[][] grid = {
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        };
+        int width = 10;
+        g.setColor(Color.RED);
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == 1) {
+                    g.fillRect(10 + j * (width+1), 10 + i * (width+1), width, width);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
+        JFrame testFrame = new JFrame();
+        testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        final Display comp = new Display();
+        comp.setPreferredSize(new Dimension(256, 256));
+        testFrame.getContentPane().add(comp, BorderLayout.CENTER);
+        testFrame.pack();
+        testFrame.setVisible(true);
+    }
+    
+    /*
+    
+    
+        JFrame testFrame = new JFrame();
+        testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        final Display comp = new Display();
+        comp.setPreferredSize(new Dimension(320, 200));
+        testFrame.getContentPane().add(comp, BorderLayout.CENTER);
+        JPanel buttonsPanel = new JPanel();
+        JButton newLineButton = new JButton("New Line");
+        JButton clearButton = new JButton("Clear");
+        buttonsPanel.add(newLineButton);
+        buttonsPanel.add(clearButton);
+        testFrame.getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+        newLineButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int x1 = (int) (Math.random()*320);
+                int x2 = (int) (Math.random()*320);
+                int y1 = (int) (Math.random()*200);
+                int y2 = (int) (Math.random()*200);
+                Color randomColor = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+                comp.addLine(x1, y1, x2, y2, randomColor);
+            }
+        });
+        clearButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comp.clearLines();
+            }
+        });
+        testFrame.pack();
+        testFrame.setVisible(true);
+    
+    */
+}
